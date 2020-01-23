9d7aa43d077145bce18caf2f73323c01e3c4199b
makutahamaka <arturoldkrom94@gmail.com>
2020-01-23 09:09:16 +0300

    header + main try. so bad, so crook

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+var overlay = document.getElementsByClassName('video__overlay');
+var vid = document.getElementsByClassName('main__video');
+
+if(overlay.addEventListener){
+    overlay.addEventListener("click", play, false)
+}else if(overlay.attachEvent){
+    overlay.attachEvent("onclick", play)
+}
+
+function play() {
+    if (vid.paused){
+        vid.play();
+        overlay.className = "o";
+    }else {
+        vid.pause();
+        overlay.className = "";
+    }
+} 
