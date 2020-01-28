8b919b12a0c73f1931474dd0f8331e7ccf48e011
makutahamaka <59964553+makutahamaka@users.noreply.github.com>
2020-01-28 22:23:38 +0300

    second block and slider try

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 var overlay = document.getElementsByClassName('video__overlay');
-var vid = document.getElementsByClassName('main__video');
+var vid = document.getElementsByClassName('section__video');
 
 if(overlay.addEventListener){
     overlay.addEventListener("click", play, false)
@@ -15,4 +15,13 @@ function play() {
         vid.pause();
         overlay.className = "";
     }
-} 
+}
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
